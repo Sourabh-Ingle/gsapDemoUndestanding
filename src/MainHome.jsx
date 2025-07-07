ec83bb1fda6008574786216bb5421173e317fcea
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-07-08 01:30:16 +0530

    implement first part of animation to text and para

--- src/MainHome.jsx
+++ src/MainHome.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 import Navbar from './components/navBarFolder/Navbar';
+import Hero from './components/hero/Hero';
 
 
 
@@ -13,17 +14,10 @@ const MainHome = () => {
   return (
       <main>
           
-          <Navbar />
-         
+         <Navbar />
+         <Hero />
 
-          <div className='flex-center'>
-              <Link to="/gsapTo">Go to gsapTo | </Link>
-              <Link to="/gsapFrom">Go to gsapFrom |</Link> 
-              <Link to="/gsapTimeline"> Go to Timeline | </Link> 
-              <Link to="/gsapStagger"> Go to Stagger | </Link> 
-              <Link to="/gsapScrollTrigger"> Go to ScrollTrigger | </Link>
-              <Link to="/gsapText"> Go to Text </Link> 
-          </div>
+          
 
 
       </main>
--- src/components/navBarFolder/Navbar.jsx
+++ src/components/navBarFolder/Navbar.jsx
@@ -1,14 +1,29 @@
-import NavbarLinks from "./NavbarLinks"
-
-
+import { useGSAP } from '@gsap/react'
+import {navLinks} from '../../../constants/constant'
+import gsap from 'gsap'
 const Navbar = () => {
+    useGSAP(() => {
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start:'bottom top'
+           }
+        }) 
+        navTween.fromTo('nav', {
+            backgroundColor:'transparent'
+        }, {
+            backgroundColor: '#00000050',
+            backgroundFilter: 'blur(10px)',
+            duration: 2,
+            ease:'power1.inOut'
+        })
+    },[])
   return (
       <nav>
           <div className="navBar">
               <a href="#" className='flex items-center gap-2'>Valet Pour</a>
               <ul>
-                  {NavbarLinks.map(link => {
-                      console.log(link.id, link.title)
+                  {navLinks.map(link => {
                       return (
                           <li key={link.id}>
                               <a href={`#${link.id}`}>{link.title}</a>
