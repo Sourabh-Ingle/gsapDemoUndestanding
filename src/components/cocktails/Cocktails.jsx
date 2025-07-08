abfe18ed73c071ea69563ac6de8775b35e141cb3
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-07-09 00:12:58 +0530

    coctails section add with gsap animation

--- src/components/cocktails/Cocktails.jsx
+++ src/components/cocktails/Cocktails.jsx
@@ -1,8 +1,83 @@
 import React from 'react'
+import { cocktailLists, mockTailLists } from '../../../constants/constant'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const Cocktails = () => {
+  useGSAP(() => {
+    const paralexTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#cocktails',
+        start: 'top 30%',
+        end: 'bottom 80%',
+        scrub:2
+       }
+    })
+    
+    paralexTimeline.from('#c-left-leaf', {
+      x:-100,y:100
+    })
+
+    paralexTimeline.from('#c-right-leaf', {
+      x: 100, y: 100
+    })
+  },[])
   return (
-    <div>Cocktails</div>
+    <section id="cocktails" className="noisy">
+
+      <img
+        src="/images/cocktail-left-leaf.png"
+        alt="left-leaf"
+        id="c-left-leaf"
+      />
+      <img
+        src="/images/cocktail-right-leaf.png"
+        alt="right-leaf"
+        id="c-right-leaf"
+      />
+
+      <div className="list">
+        <div className="popular">
+          <h2>Most Popular Cocktails</h2>
+
+          <ul>
+            {
+              cocktailLists.map(({ name, country, price,detail }) => {
+                return (
+                  <li key={name}>
+                    <div className="md:me-28">
+                      <h3>{name}</h3>
+                      <p>{country} | {detail }</p>
+                    </div>
+                    <span>- {price}</span>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </div>
+
+        <div className="loved">
+          <h2>Most loved Mocktails</h2>
+
+          <ul>
+            {
+              mockTailLists.map(({ name, country, price, detail }) => {
+                return (
+                  <li key={name}>
+                    <div className="me-28">
+                      <h3>{name}</h3>
+                      <p>{country} | {detail}</p>
+                    </div>
+                    <span>- {price}</span>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </div>
+      </div>
+    </section>
   )
 }
 
